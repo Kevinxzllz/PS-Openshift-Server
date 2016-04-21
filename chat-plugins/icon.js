24a03d219bd9bb7f1b95518a8af0f1e545902a8c
kevinxzllz <kevinfalcon1509@gmail.com>
2016-04-21 20:24:37 +0000

    Añado Plugin de Iconos

---
+++ chat-plugins/icon.js
@@ -0,0 +1,29 @@
+/*
+* Creditos: MasterFloat (Creador), Kevinxzllz (Adaptacion a Openshift y Traduccion)
+*/
+
+var fs = require('fs');
+var selectors;
+
+function writeIconCSS() {
+   fs.appendFile( DATA_DIR + 'custom.css', selectors);
+}
+
+exports.commands = {
+    seticon: function (target, room, user) {
+        if (!this.can('ban')) return this.errorReply("Accedo denegado.");
+
+        var args = target.split(',');if (args.length < 3) return this.parse('/help seticon');
+        var username = toId(args.shift());
+        var image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
+        selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
+        args.forEach(function (room) {
+            selectors += ', #' + toId(room) + '-userlist-user-' + username;
+        });
+        selectors += ' { \n' + '    ' + image +  '\n  }';
+
+        this.privateModCommand("(" + user.name + " le ha establecido un icono a " + username + ")");
+        writeIconCSS();
+    },
+    seticonhelp: ["/seticon [usuario], [imagen], [room 1], [room 2], etc. - Recuerda debe ser una imagen 32x32."]
+};
