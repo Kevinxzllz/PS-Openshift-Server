c8deec8d086ceedf9baefdc2b7ce2ca8b11059e8
Kevinxzllz <kevinfalcon1509@gmail.com>
2016-05-18 19:04:45 -0430

    Agrego "pmall y pmstaff"

--- chat-plugins/custom.js
+++ chat-plugins/custom.js
@@ -1,28 +1,35 @@
 
 exports.commands = {
-	/*stafflist: 'authlist',
-	authlist: function (target, room, user, connection) {
-		var rankLists = {};
-		for (var u in Users.usergroups) {
-			var rank = Users.usergroups[u][0];
-			var name = Users.usergroups[u].slice(1);
-			if (!rankLists[rank]) rankLists[rank] = [];
-			if (name) name = name.replace("\n", "").replace("\r", "");
-			rankLists[rank].push(name);
-		}
-		var buffer = [];
-		Object.keys(rankLists).sort(function (a, b) {
-			return Config.groups[b].rank - Config.groups[a].rank;
-		}).forEach(function (r) {
-			buffer.push(Config.groups[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
-		});
+	
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
 
-		if (!buffer.length) {
-			buffer = "This server has no auth.";
-			return connection.popup("This server has no auth.");
+		var pmName = ' InFo.PM [No Responder]';
+
+		for (var i in Users.users) {
+			var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+			Users.users[i].send(message);
 		}
-		connection.popup(buffer.join("\n\n"));
-	},*/
+	},
+	pmallhelp: ["/pmall [message] - Envia un MP a todos en el Server"],
+	
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		var pmName = ' InFo.Staff [No Responder]';
+
+		for (var i in Users.users) {
+			if (Users.users[i].isStaff) {
+				Users.users[i].send('|pm|' + pmName + '|' + Users.users[i].group + Users.users[i].name + '|' + target);
+			}
+		}
+	},
+	pmallstaffhelp: ["/pmallstaff [message] - Envia un mensaje a Todo el miembro del Staff ."],
 	
 	postimage: 'image',
 	image: function (target, room, user) {
